9c995f77534653790b0859fe40fee8fb6eb5f7a0
RSireesha <ramireddysireesha20@gmail.com>
2024-08-27 15:47:43 +0530

    some more examples

--- 01_variables.py
+++ 01_variables.py
@@ -9,3 +9,21 @@ a = 42 # This is a integer variable
 
 # Print the value that is stored inside the variable
 print(a)
+
+# Float value
+b = 42.345
+print(b)
+
+# Boolean
+c = True # true
+
+# Strings
+d = 'This is a string'
+e = "This is also a string"
+f = """
+This is a multi line string
+"""
+
+# Today's weather is nice
+d = 'Today\'s weather is nice'
+print(d)
